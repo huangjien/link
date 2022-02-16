3de31f75f7a632e6362e0473d134defa07948859
jien.huang <huangjien@gmail.com>
2022-02-16 14:34:55 +0000

    debug

--- .github/pr_validation.py
+++ .github/pr_validation.py
@@ -1,5 +1,11 @@
 import sys
 
+def validation(argv):
+    print(argv[1])
+    print(argv[2])
+
 if __name__ == '__main__':
-    print ('Validation Finished')
+    print ('Start Validation...')
+    validation(sys.argv[1:])
+    print ('Validation Finished!')
     sys.exit(0)
